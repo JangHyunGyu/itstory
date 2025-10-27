9625c1dd69fd52a639f267470d348e30385b7de2
yama5993 <yama5993@nate.com>
2025-10-27 14:14:08 +0900

    itstory 폴더의 모든 HTML(index, networks, computers, web 시리즈 등)은 gtag 로더 URL이 동일한 새 ID를 참조하도록 일괄 업데이트했습니다.

--- assets/js/ga.js
+++ assets/js/ga.js
@@ -1,7 +1,7 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'G-MTBCHM8DYM', {
+gtag('config', 'G-EDBPXC8PL7', {
 	linker: {
 		domains: ['latindance.kr', 'archerlab.dev', 'itstory.archerlab.dev', 'walkwithme.archerlab.dev']
 	}
