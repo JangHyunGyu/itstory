b09f076158f10fc6974a652a91ff5a85424c75fa
yama5993 <yama5993@nate.com>
2025-12-23 17:52:42 +0900

    feat: Update Google Analytics tracking ID and WCS "wa" value in HTML and JS files

--- assets/js/ga.js
+++ assets/js/ga.js
@@ -1,7 +1,13 @@
+(function() {
+	var script = document.createElement('script');
+	script.async = true;
+	script.src = 'https://www.googletagmanager.com/gtag/js?id=G-EDBPXC8PL7';
+	document.head.appendChild(script);
+})();
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'G-KDKK3ZZK50', {
+gtag('config', 'G-EDBPXC8PL7', {
 	linker: {
 		domains: ['latindance.kr', 'archerlab.dev', 'itstory.archerlab.dev', 'chatbot.archerlab.dev', 'golf.archerlab.dev', 'cupid.archerlab.dev']
 	}
